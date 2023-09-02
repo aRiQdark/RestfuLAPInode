fec6c1fc2d43644f07b4c8293f235cecee31e099
Muhammad Ariq Pratama <muhammadariq105@gmail.com>
2023-09-02 08:36:15 +0700

    menampilkan semua data

--- controller.js
+++ controller.js
@@ -1,8 +1,22 @@
 'user strict';
 
 var response = require('./res');
-var connect = require('./koneksi');
+var Connection = require('./koneksi');
+// const Connection = require('mysql/lib/Connection');
 
 exports.index = function(req,res){
     response.ok('aplikasi rest api berjalan',res)
+}
+
+exports.tampilsemuadata = function(req,res){
+
+    Connection.query(
+        'SELECT * FROM mahasiswa',function(error,rows,fields){
+            if (error) {
+                Connection.log(error);
+            }else{
+                response.ok(rows,res)
+            }
+        }
+    )
 }
--- router.js
+++ router.js
@@ -3,5 +3,7 @@
 module.exports =function(app){
     var jsonku = require('./controller');
 
-    app.route('/').get(jsonku.index)
+    app.route('/').get(jsonku.index);
+
+    app.route('/tampil').get(jsonku.tampilsemuadata);
 }
