27ed4609bc30dc569c66a1436b1fec823d4c2296
Muhammad Ariq Pratama <muhammadariq105@gmail.com>
2023-09-17 23:55:01 +0700

    tes

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+
 const app = express();
 
+const port = process.env.PORT || 3000;
 
-//parse body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-//dibuat setelah membuat routes
-var routes = require('./router');
-routes(app);
 app.get("/", (req, res) => res.send("Hello world"));
 app.get("/ping", (req, res) => res.send("Pong"));
 app.get("/felix", (req, res) => res.send("Liawi"));
 
-app.listen(3000, () => {
-    console.log(`Server started on port`);
- });
-
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
---
+++ server.js
@@ -0,0 +1,20 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+
+//parse body
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//dibuat setelah membuat routes
+var routes = require('./router');
+routes(app);
+app.get("/", (req, res) => res.send("Hello world"));
+app.get("/ping", (req, res) => res.send("Pong"));
+app.get("/felix", (req, res) => res.send("Liawi"));
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+ });
+
