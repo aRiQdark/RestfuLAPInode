9d731188f299f594019db7ed3364528235f238c7
Muhammad Ariq Pratama <muhammadariq105@gmail.com>
2023-09-18 00:09:58 +0700

    tes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello world"));
-app.get("/ping", (req, res) => res.send("Pong"));
-app.get("/felix", (req, res) => res.send("Liawi"));
+var routes = require('./router');
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
